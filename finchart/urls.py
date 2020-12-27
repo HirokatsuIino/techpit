83f73b8b491133a47b0bed801183b14546991dda
hirokatsuiino <letterbox6212>
2020-12-27 17:01:26 +0900

    [ADD]トップページ設定、ルーティング設定

--- finchart/urls.py
+++ finchart/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.urls import include
+
+from . import views
+
+app_name = 'finchart'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('finchart.urls')),
+    path('', views.IndexTemplateView.as_view(), name='index'),
 ]
