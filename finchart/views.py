f6a682fc63402c97bec1f8c418e902149bf14d5b
hirokatsuiino <letterbox6212>
2020-12-28 11:17:46 +0900

    [ADD]CompanyViewの詳細画面用の関数定義

--- finchart/views.py
+++ finchart/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 from django.views.generic import TemplateView
+from django.views.generic import DetailView
+from .models import Company
 from .models import Fstatement
 
 
@@ -15,6 +17,19 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         fstatement_list = Fstatement.objects.all().order_by('company')
         params = {
+            'fstatement_list': fstatement_list,
+        }
+        return params
+
+
+class CompanyView(DetailView):
+    model = Company
+
+    def get_context_data(self, **kwargs):
+        company_name = kwargs['object'].name
+        fstatement_list = Fstatement.objects.filter(company=kwargs['object']).order_by('-fiscal_year')[:4]
+        params = {
+            'company_name': company_name,
             'fstatement_list': fstatement_list,
         }
         return params
