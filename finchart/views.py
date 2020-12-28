82257d5dee580ae86cd22186821c4675f337ad39
hirokatsuiino <letterbox6212>
2020-12-28 10:35:11 +0900

    [ADD]jQuery Datatables導入てのトップページ作成

--- finchart/views.py
+++ finchart/views.py
@@ -3,7 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import generic
 from django.views.generic import TemplateView
+from .models import Fstatement
 
 
-class IndexTemplateView(TemplateView):
+# class IndexTemplateView(TemplateView):
+#     template_name = 'finchart/index.html'
+
+class IndexView(TemplateView):
     template_name = 'finchart/index.html'
+
+    def get_context_data(self, **kwargs):
+        fstatement_list = Fstatement.objects.all().order_by('company')
+        params = {
+            'fstatement_list': fstatement_list,
+        }
+        return params
