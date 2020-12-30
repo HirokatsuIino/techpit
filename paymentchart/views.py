f58e1809951e896dc2f026506b99c408beaa644b
hirokatsuiino <letterbox6212>
2020-12-30 17:41:37 +0900

    [ADD]PaymentItemテーブルから読み込み

--- paymentchart/views.py
+++ paymentchart/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import PaymentItem
 
 
 # Create your views here.
 class IndexTemplateView(TemplateView):
     template_name = 'paymentchart/index.html'
+
+    def get_context_data(self, **kwargs):
+        fstatement_list = PaymentItem.objects.all()
+        params = {
+            'fstatement_list': fstatement_list,
+        }
+        return params
