889168a1e38fe6b1db51148b7f3f5a9af5d5f062
hirokatsuiino <letterbox6212>
2020-12-30 17:32:32 +0900

    [ADD]支払い項目テーブル作成

--- paymentchart/models.py
+++ paymentchart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PaymentItem(models.Model):
+    payment_name = models.CharField("支払い項目", max_length=100, blank=True, default='')
+    payment_value = models.IntegerField("支払い額", default=0)
