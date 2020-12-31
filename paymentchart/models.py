9c337e51f5f787d0124ef372c6e65eb2b7263ce3
hirokatsuiino <letterbox6212>
2020-12-31 12:21:24 +0900

    [ADD]paymentitemテーブルに、dataを追加

--- paymentchart/models.py
+++ paymentchart/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 
 class PaymentItem(models.Model):
+    payment_data = models.DateField("決済月日", default=datetime.date.today)
     payment_name = models.CharField("支払い項目", max_length=100, blank=True, default='')
     payment_value = models.IntegerField("支払い額", default=0)
