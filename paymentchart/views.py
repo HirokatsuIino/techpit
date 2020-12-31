33a408cba4c20cb493e57a4b6757f1962415a191
hirokatsuiino <letterbox6212>
2020-12-31 10:57:35 +0900

    [MODEF]viewのclassをTemplateViewからListViewに変更

--- paymentchart/views.py
+++ paymentchart/views.py
@@ -1,11 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic import ListView
 from .models import PaymentItem
+from .forms import PaymentItemForm
+from django.views.generic.edit import ModelFormMixin
 
 
 # Create your views here.
-class IndexTemplateView(TemplateView):
-    template_name = 'paymentchart/index.html'
+class PaymentItemListView(ListView):
+    model = PaymentItem
+    # form_class = PaymentItemForm
+    # template_name = 'paymentchart/paymentitem_list.html'
 
     def get_context_data(self, **kwargs):
         payment_list = PaymentItem.objects.all()
@@ -13,3 +20,18 @@ class IndexTemplateView(TemplateView):
             'payment_list': payment_list,
         }
         return params
+
+    # def form_valid(self, form):
+    #     result = form.save(commit=False)
+    #     result.payment_name = self.get_object()
+    #     result.payment_value = self.get_object()
+    #     result.save()
+    #     return HttpResponseRedirect(self.request.path_info)
+    #
+    # def post(self, request, *args, **kwargs):
+    #     form = self.get_form()
+    #     if form.is_valid():
+    #         return self.form_valid(form)
+    #     else:
+    #         self.object = self.get_object()
+    #         return self.form_invalid(form)
